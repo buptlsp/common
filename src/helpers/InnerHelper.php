8c68ce67860351cb42145bd3afd6eb3b34dd09c1
yangchao <yangchao@ethercap.com>
2016-11-30 16:13:10 +0800

    add url

--- src/helpers/InnerHelper.php
+++ src/helpers/InnerHelper.php
@@ -17,20 +17,24 @@ class InnerHelper extends \lspbupt\curl\CurlHttp
                 'message' => '网络错误',
                 'data' => [],
             ];
+
+            $reqUrl = sprintf(" [%s] ", $curlHttp->getUrl());
+            $category = "curl." . $this->appkey;
+
             if($code == 200) {
                 $ret = json_decode($response, true);
                 if(empty($ret)) {
-                    Yii::warning("error!".$response, "curl.admin");
+                    Yii::warning("error!" . $reqUrl . $response, $category);
                     return $data;
                 }
                 if(!empty($ret['code'])) {
-                    Yii::warning("error!".$response, "curl.admin");
+                    Yii::warning("error!" . $reqUrl . $response, $category);
                     return $ret;
                 }
-                Yii::info("ok!", "curl.admin");
+                Yii::info("ok!" . $reqUrl, $category);
                 return $ret;
             }
-            Yii::error("error", "curl.admin");
+            Yii::error("error" . $reqUrl,  $category);
             return $data;
         };
     }
